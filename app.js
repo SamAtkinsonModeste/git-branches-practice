1983fabba2cb1e05294b0289a9ccc9eeca167b29
Sam Atkinson Modeste <sammodeste@yahoo.com>
2026-06-12 14:33:33 +0100

    Wand property added

--- app.js
+++ app.js
@@ -9,6 +9,25 @@ const characters = [
 const characterBtn = document.querySelector("#character-btn");
 const characterCard = document.querySelector("#character-card");
 
+const characters = [
+  {
+    character: "Harry Potter",
+    house: "Gryffindor",
+    wand: "11 inches, holly, phoenix feather core",
+    father: "James Potter",
+    mother: "Lily Potter",
+    pet: " Hedwig",
+  },
+  {
+    character: "Hermione Granger",
+    house: "Gryffindor",
+    wand: "10¾ inches, vine wood, dragon heartstring core",
+    father: "Daniel Granger",
+    mother: "Emma Granger",
+    pet: "Crookshanks",
+  },
+];
+
 function showRandomCharacter() {
   const randomIndex = Math.floor(Math.random() * characters.length);
 
